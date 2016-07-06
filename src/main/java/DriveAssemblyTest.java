b15b65fa21137fefcb2e833efc6af8a33e350f9f
Matthew Wood <mattcow12@googlemail.com>
2016-07-06 21:51:55 +0100

    Added drive assembly test class

--- src/main/java/DriveAssemblyTest.java
+++ src/main/java/DriveAssemblyTest.java
@@ -10,10 +10,10 @@ import motors.Motor;
  */
 public class DriveAssemblyTest
 {
-    public static final Pin LEFT_MOTOR_A = RaspiPin.GPIO_00;
-    public static final Pin LEFT_MOTOR_B = RaspiPin.GPIO_01;
-    public static final Pin RIGHT_MOTOR_A = RaspiPin.GPIO_02;
-    public static final Pin RIGHT_MOTOR_B = RaspiPin.GPIO_03;
+    public static final Pin LEFT_MOTOR_A = RaspiPin.GPIO_02;
+    public static final Pin LEFT_MOTOR_B = RaspiPin.GPIO_03;
+    public static final Pin RIGHT_MOTOR_A = RaspiPin.GPIO_00;
+    public static final Pin RIGHT_MOTOR_B = RaspiPin.GPIO_01;
 
 
     public static void main(String[] args) throws InterruptedException
@@ -37,7 +37,6 @@ public class DriveAssemblyTest
 
     public static void cycleSpeed(DriveAssembly assembly, int delay) throws InterruptedException
     {
-
         for(int i = 10; i<100; i++)
         {
             assembly.setSpeed((float)i/100);
