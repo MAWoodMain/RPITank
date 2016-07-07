cc866ced67db7ef0baec5fe4224eca3a8a25284e
Matthew Wood <mattcow12@googlemail.com>
2016-07-07 22:45:53 +0100

    working on MPU9250 Currently outputting value unsure if accurate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,13 @@
 
 import com.pi4j.io.i2c.I2CFactory;
 import javafx.geometry.Point3D;
-import sensors.HMC5883L;
 import sensors.MPU9250;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
 
 /**
  * RPITank
@@ -16,17 +19,17 @@ public class Main
     public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException, InterruptedException
     {
         MPU9250 mpu9250 = new MPU9250(104);
-        int i = 0;
-           // System.out.println("Gyro: " + mpu9250.getGyro());
+        String format = "%6.2f";
 
-            i++;
-            Point3D gyro = mpu9250.getGyro();
+        for(int x=0; x<1000; x++)
+        {
             Point3D mag = mpu9250.getMag();
-            Point3D accel = mpu9250.getAccel();
-            /*System.out.println(i + ", " + gyro.getX() + ", " + gyro.getY() + ", " + gyro.getZ() + ", " +
-            mag.getX() + ", " + mag.getY() + ", " + mag.getZ() + ", " +
-            accel.getX() + ", " + accel.getY() + ", " + accel.getZ());*/
-            System.out.println(mag);
+            Point3D acc = mpu9250.getAccel();
+            Point3D gyr = mpu9250.getGyro();
+            System.out.print(  "mag: " + String.format(format,mag.getX()) + ", " + String.format(format,mag.getY()) + ", " + String.format(format,mag.getZ()) + " ");
+            System.out.print(  "acc: " + String.format(format,acc.getX()) + ", " + String.format(format,acc.getY()) + ", " + String.format(format,acc.getZ()) + " ");
+            System.out.println("gyr: " + String.format(format,gyr.getX()) + ", " + String.format(format,gyr.getY()) + ", " + String.format(format,gyr.getZ()) + " ");
             Thread.sleep(100);
+        }
     }
 }
--- src/main/java/sensors/MPU9250.java
+++ src/main/java/sensors/MPU9250.java
@@ -3,6 +3,7 @@ package sensors;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
+import com.sun.deploy.util.ArrayUtil;
 import javafx.geometry.Point3D;
 
 import java.io.IOException;
@@ -23,20 +24,54 @@ public class MPU9250
 
     private final I2CDevice mpu9250;
 
-    public MPU9250(int address) throws I2CFactory.UnsupportedBusNumberException, IOException
+    public MPU9250(int address) throws I2CFactory.UnsupportedBusNumberException, IOException, InterruptedException
     {
         // get device
         I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
         mpu9250 = bus.getDevice(address);
 
-       mpu9250.read(0);
+        mpu9250.write(0x6B,(byte)0x80); // reset device
+        Thread.sleep(10);
+        mpu9250.write(0x6B,(byte)0x01); // clock source
+        Thread.sleep(10);
+        mpu9250.write(0x6C,(byte)0x00); // enable acc and gyro
+        Thread.sleep(10);
+        mpu9250.write(0x1A,(byte)0x01); // use DLPF set gyroscope bandwidth 184Hz
+        Thread.sleep(10);
+        mpu9250.write(0x1B,(byte)0x18); // +-2000dps
+        Thread.sleep(10);
+        mpu9250.write(0x1C,(byte)0x08); // +-4G
+        Thread.sleep(10);
+        mpu9250.write(0x1D,(byte)0x09); // set acc data rates, enable acc LPF, bandwidth 184Hz
+        Thread.sleep(10);
+        mpu9250.write(0x37,(byte)0x30); //
+        Thread.sleep(10);
+        mpu9250.write(0x6A,(byte)0x20); // I2C master mode
+        Thread.sleep(10);
+        mpu9250.write(0x24,(byte)0x0D); // I2C configuration multi-master IIC 400KHz
+        Thread.sleep(10);
 
-        // configure device
-        mpu9250.write(27,(byte)0x18); // set gyroscope to full scale
-        mpu9250.write(28,(byte)0x18); // set accelerometer to full scale
-        mpu9250.write(55,(byte)0x00); // set pass mode for magnetometer
-        mpu9250.write(10,(byte)0x06); // set magnetometer mode 2:continuous 1 6: continuous 2 1: single
-        //mpu9250.write(11,(byte)0x00); // disable reset
+        mpu9250.write(0x25,(byte)0x0C); // set the I2C slave address of AK8963
+        Thread.sleep(10);
+
+        mpu9250.write(0x26,(byte)0x0B); // I2C slave 0 register address from where to begin
+        Thread.sleep(10);
+        mpu9250.write(0x63,(byte)0x01); // reset AK8963
+        Thread.sleep(10);
+        mpu9250.write(0x27,(byte)0x81); // Enable I2C and set 1 byte
+        Thread.sleep(10);
+
+        mpu9250.write(0x26,(byte)0x0A); // I2C slave 0 register address from where to begin
+        Thread.sleep(10);
+        mpu9250.write(0x63,(byte)0x12); // register value to continuous measurement 16 bit
+        Thread.sleep(10);
+        mpu9250.write(0x27,(byte)0x81); // Enable I2C and set 1 byte
+        Thread.sleep(10);
+
+        mpu9250.write(0x1B, (byte)0x18); // +-2000dps
+        Thread.sleep(10);
+        mpu9250.write(0x1C, (byte)0x08); // +-4G
+        Thread.sleep(100);
     }
 
     private void updateAccel()
@@ -55,6 +90,7 @@ public class MPU9250
     {
         try
         {
+
             this.gyro = new Point3D(getData(67),getData(69),getData(71));
         } catch (IOException e)
         {
@@ -62,17 +98,22 @@ public class MPU9250
         }
     }
 
-    private void updateMag()
+    private void updateMag() throws InterruptedException
     {
         try
         {
-            /*for(int i = 73; i<=96; i++)
-            {
-                System.out.println(i + " : " + Integer.toBinaryString(mpu9250.read(i)));
-            }
-            System.out.println("55 : " + Integer.toBinaryString(mpu9250.read(55)));*/
-            this.mag = new Point3D(getData(5),getData(3),getData(7));
-            mpu9250.read(9);
+            mpu9250.write(0x25,(byte)((byte)0x0c|(byte)0x80));
+            mpu9250.write(0x26, (byte)0x03);
+            mpu9250.write(0x27, (byte)0x87);
+
+            Thread.sleep(10);
+
+            byte[] data = new byte[7];
+            mpu9250.read(0x49,data,0,7);
+            this.mag = new Point3D(
+                    (float)(data[0]<<8 | data[1]),
+                    (float)(data[2]<<8 | data[3]),
+                    (float)(data[4]<<8 | data[5]));
         } catch (IOException e)
         {
             e.printStackTrace();
@@ -93,8 +134,8 @@ public class MPU9250
     private float getData(int address) throws IOException
     {
         byte high = (byte)mpu9250.read(address);
-        byte low = (byte)mpu9250.read(address);
-        return ((float)(high<<8 &0xFF00 | low&0xFF))/100; // construct 16 bit integer from two bytes
+        byte low = (byte)mpu9250.read(address + 1);
+        return ((float)(high<<8 | low)); // construct 16 bit integer from two bytes
     }
 
     public Point3D getAccel()
@@ -111,7 +152,13 @@ public class MPU9250
 
     public Point3D getMag()
     {
-        updateMag();
+        try
+        {
+            updateMag();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
         return mag;
     }
 
@@ -119,5 +166,13 @@ public class MPU9250
     {
         updateTemp();
         return temp;
+    }
+
+    public float getHeading()
+    {
+        double heading = 0f;
+        heading = Math.atan2(mag.getY(), mag.getX());
+        if (heading < 0) heading += (2 * Math.PI);
+        return (float) Math.toDegrees(heading);
     }
 }
