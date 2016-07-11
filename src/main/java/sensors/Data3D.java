e9643e12d40e1b351df9e7747f7e56e71fe512f9
gjwo <grahamjwood@gmail.com>
2016-07-11 18:56:38 +0100

    Ungenericed data3D and added normalise

--- src/main/java/sensors/Data3D.java
+++ src/main/java/sensors/Data3D.java
@@ -4,39 +4,51 @@ package sensors;
  * RPITank - sensors
  * Created by matthew on 10/07/16.
  */
-public class Data3D<T> {
+public class Data3D {
 
-    private T x;
-    private T y;
-    private T z;
+    private float x;
+    private float y;
+    private float z;
 
-    public Data3D(T x, T y, T z) {
+    public Data3D(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public T getX() {
+    public float getX() {
         return x;
     }
 
-    public T getY() {
+    public float getY() {
         return y;
     }
 
-    public T getZ() {
+    public float getZ() {
         return z;
     }
 
-	public void setX(T x) {
+	public void setX(float x) {
 		this.x = x;
 	}
 
-	public void setY(T y) {
+	public void setY(float y) {
 		this.y = y;
 	}
 
-	public void setZ(T z) {
+	public void setZ(float z) {
 		this.z = z;
+	}
+	public void normalize(){
+		float norm;
+		// Normalise accelerometer measurement
+		norm = (float)Math.sqrt(x*x + y*y + z*z);
+		if (norm == 0.0f)
+			return; // handle NaN
+		norm = 1.0f / norm;
+		x *= norm;
+		y *= norm;
+		z *= norm;
+		
 	}
 }
