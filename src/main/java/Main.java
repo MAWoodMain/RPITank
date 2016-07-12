8f533135a6553e82bcc1ccda4c4fe8c09578fcdb
Matthew Wood <mattcow12@googlemail.com>
2016-07-12 22:52:40 +0100

    Debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,20 +15,11 @@ public class Main
     public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException, InterruptedException
     {
         MPU9250 mpu9250 = new MPU9250(100,100);
-        new Thread(mpu9250).start();
-        String format = "%+04.3f";
+        Thread sensor = new Thread(mpu9250);
+        sensor.start();
 
-        Thread.sleep(100);
+        Thread.sleep(5000);
 
-        for(int x=0; x<1000; x++)
-        {
-            Data3D mag = mpu9250.getLatestGaussianData();
-            Data3D acc = mpu9250.getLatestAcceleration();
-            Data3D gyr = mpu9250.getLatestRotationalAcceleration();
-            System.out.print(  "acc: " + acc.toString());
-            System.out.print(  "gyr: " + gyr.toString());
-            System.out.println("mag: " + mag.toString());
-            Thread.sleep(10);
-        }
+        sensor.interrupt();
     }
 }
