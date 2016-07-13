c67070b57a9141a8a3ca0cddbb14328edab2f91e
Matthew Wood <mattcow12@googlemail.com>
2016-07-13 13:13:33 +0100

    Added non static version of integrate and cleaned function

--- src/main/java/sensors/dataTypes/TimestampedData3D.java
+++ src/main/java/sensors/dataTypes/TimestampedData3D.java
@@ -5,8 +5,8 @@ package sensors.dataTypes;
  * Created by MAWood on 11/07/2016.
  */
 public class TimestampedData3D extends Data3D
-{    
-	public static final long NANOS_PER_SEC = 1000000000;
+{
+    public static final long NANOS_PER_SEC = 1000000000;
     public final long nanoTime;
 
     public TimestampedData3D(float x, float y, float z, long nanoTime)
@@ -30,27 +30,40 @@ public class TimestampedData3D extends Data3D
         super(data.getX(),data.getY(),data.getZ());
         this.nanoTime = data.nanoTime;
     }
-   
-	public String toString()
-	{
-		String format = "%+04.4f";
-		return 	" t: " + String.format(format,(float)(nanoTime/NANOS_PER_SEC)) +
-				" x: " + String.format(format,this.getX()) +
-				" y: " + String.format(format,this.getY()) +
-				" z: " + String.format(format,this.getZ());
-	}
-	
-	public static TimestampedData3D integrate(TimestampedData3D sampleT, TimestampedData3D sampleTm1 )
-	{
-	   float deltaT;
-	   TimestampedData3D integral = new TimestampedData3D(sampleT); // preserve timestamp in result
-	   deltaT = (float)(sampleT.nanoTime-sampleTm1.nanoTime)/(float)TimestampedData3D.NANOS_PER_SEC; // time difference between samples in seconds
-	   
-	   integral.setX((float)(sampleT.getX()+sampleTm1.getX())/2f*deltaT);  //Trapezoidal area, average height X deltaT
-	   integral.setY((float)(sampleT.getY()+sampleTm1.getY())/2f*deltaT);
-	   integral.setZ((float)(sampleT.getZ()+sampleTm1.getZ())/2f*deltaT);
-	   
-	   return integral;
-	}
+
+    public String toString()
+    {
+        String format = "%+04.4f";
+        return 	" t: " + String.format(format,(float)(nanoTime/NANOS_PER_SEC)) +
+                " x: " + String.format(format,this.getX()) +
+                " y: " + String.format(format,this.getY()) +
+                " z: " + String.format(format,this.getZ());
+    }
+
+    public static TimestampedData3D integrate(TimestampedData3D sampleT, TimestampedData3D sampleTm1 )
+    {
+        float deltaT = (float)(sampleT.nanoTime-sampleTm1.nanoTime)/(float)TimestampedData3D.NANOS_PER_SEC; // time difference between samples in seconds
+
+        TimestampedData3D integral = new TimestampedData3D(sampleT); // preserve timestamp in result
+
+        integral.setX((sampleT.getX()+sampleTm1.getX())/2f*deltaT);  //Trapezoidal area, average height X deltaT
+        integral.setY((sampleT.getY()+sampleTm1.getY())/2f*deltaT);
+        integral.setZ((sampleT.getZ()+sampleTm1.getZ())/2f*deltaT);
+
+        return integral;
+    }
+
+    public TimestampedData3D integrate(TimestampedData3D sampleTm1 )
+    {
+        float deltaT = (float)(this.nanoTime-sampleTm1.nanoTime)/(float)TimestampedData3D.NANOS_PER_SEC; // time difference between samples in seconds
+
+        TimestampedData3D integral = new TimestampedData3D(this); // preserve timestamp in result
+
+        integral.setX((this.getX()+sampleTm1.getX())/2f*deltaT);  //Trapezoidal area, average height X deltaT
+        integral.setY((this.getY()+sampleTm1.getY())/2f*deltaT);
+        integral.setZ((this.getZ()+sampleTm1.getZ())/2f*deltaT);
+
+        return integral;
+    }
 
 }
