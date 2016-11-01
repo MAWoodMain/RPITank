03af2cd2b616cde20f0117f55e937190f060ed9d
gjwo <grahamjwood@gmail.com>
2016-11-01 09:34:51 +0000

    Refactor printRegister -> PrintByteRegister

--- src/main/java/devices/sensorImplementations/MPU9250/MPU9250.java
+++ src/main/java/devices/sensorImplementations/MPU9250/MPU9250.java
@@ -517,6 +517,7 @@ public class MPU9250 extends NineDOF
         mpu9250.read(Registers.TEMP_OUT_H.getAddress(),2);  // Read again to trigger
         therm.add((float)(short)((rawData[0] << 8) | rawData[1]));  // Turn the MSB and LSB into a 16-bit value
     }
+    
     /**
      * Reads the specified number of 16 bit Registers from a given device and address
      * @param address 	- the start address for the read
@@ -545,7 +546,7 @@ public class MPU9250 extends NineDOF
     	String s = String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
     	return s;  	
     }
-    public void printRegister(Registers r)
+    public void printByteRegister(Registers r)
     {
     	byte rv = 0;
     	try {
@@ -559,27 +560,27 @@ public class MPU9250 extends NineDOF
     
     public void outputConfigRegisters()
     {
-    	printRegister(Registers.CONFIG);
-    	printRegister(Registers.GYRO_CONFIG);
-    	printRegister(Registers.ACCEL_CONFIG);
-    	printRegister(Registers.ACCEL_CONFIG2);
-    	printRegister(Registers.LP_ACCEL_ODR);
-    	printRegister(Registers.WOM_THR);
-    	printRegister(Registers.MOT_DUR);
-    	printRegister(Registers.ZMOT_THR);
-    	printRegister(Registers.FIFO_EN);
-    	printRegister(Registers.I2C_MST_CTRL);
-    	printRegister(Registers.I2C_MST_STATUS);
-    	printRegister(Registers.INT_PIN_CFG);
-    	printRegister(Registers.INT_ENABLE);
-    	printRegister(Registers.INT_STATUS);
-    	printRegister(Registers.I2C_MST_DELAY_CTRL);
-    	printRegister(Registers.SIGNAL_PATH_RESET);
-    	printRegister(Registers.MOT_DETECT_CTRL);
-    	printRegister(Registers.USER_CTRL);
-    	printRegister(Registers.PWR_MGMT_1);
-    	printRegister(Registers.PWR_MGMT_2);
-    	printRegister(Registers.WHO_AM_I_MPU9250);
-    	printRegister(Registers.SMPLRT_DIV);
+    	printByteRegister(Registers.CONFIG);
+    	printByteRegister(Registers.GYRO_CONFIG);
+    	printByteRegister(Registers.ACCEL_CONFIG);
+    	printByteRegister(Registers.ACCEL_CONFIG2);
+    	printByteRegister(Registers.LP_ACCEL_ODR);
+    	printByteRegister(Registers.WOM_THR);
+    	printByteRegister(Registers.MOT_DUR);
+    	printByteRegister(Registers.ZMOT_THR);
+    	printByteRegister(Registers.FIFO_EN);
+    	printByteRegister(Registers.I2C_MST_CTRL);
+    	printByteRegister(Registers.I2C_MST_STATUS);
+    	printByteRegister(Registers.INT_PIN_CFG);
+    	printByteRegister(Registers.INT_ENABLE);
+    	printByteRegister(Registers.INT_STATUS);
+    	printByteRegister(Registers.I2C_MST_DELAY_CTRL);
+    	printByteRegister(Registers.SIGNAL_PATH_RESET);
+    	printByteRegister(Registers.MOT_DETECT_CTRL);
+    	printByteRegister(Registers.USER_CTRL);
+    	printByteRegister(Registers.PWR_MGMT_1);
+    	printByteRegister(Registers.PWR_MGMT_2);
+    	printByteRegister(Registers.WHO_AM_I_MPU9250);
+    	printByteRegister(Registers.SMPLRT_DIV);
     }
 }
